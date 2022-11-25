505043d178cb45417a57b2a82ac392fe6840aa65
MierkiewiczN <natalia.mmie@gmail.com>
2022-11-25 17:53:27 +0100

    Further style changes

--- components/Flag.js
+++ components/Flag.js
@@ -1,5 +1,5 @@
 const Flag = ({ code }) => (
-    <span className={`fib fi-${code} mr-1`}
+    <span className={`fib fi-${code} mr-1 mt-1`}
         style={{boxShadow: '0 0 1px #ddd, 0 0 1px #ddd, 0 0 1px #ddd', height:'19px', width:'25px'}}/>
 )
 
